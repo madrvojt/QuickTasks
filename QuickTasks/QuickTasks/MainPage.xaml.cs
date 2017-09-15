88ac959c417a0b40ba606c3be7494304e71c7b42
Vojtěch Mádr <vojtech.madr@worklio.com>
2017-09-15 12:49:31 +0200

    Update code for Thirt_step

--- QuickTasks/QuickTasks/MainPage.xaml.cs
+++ QuickTasks/QuickTasks/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,15 +10,24 @@ namespace QuickTasks
 {
 	public partial class MainPage : ContentPage
 	{
+		public ObservableCollection<string> tasks;
+
 		public MainPage()
 		{
 			InitializeComponent();
-			List<string> tasks = new List<string>();
+			tasks = new ObservableCollection<string>();
 			tasks.Add("Create Xamarin Apps");
 			tasks.Add("Join XMDG.cz on Facebook and Twitter");
 			tasks.Add("Enjoy Xamarin Prague Days");
 
 			QuickTaskListView.ItemsSource = tasks;
+
+
+			TaskButton.Clicked += (s, e) => {
+				var text = TaskExtry.Text;
+				tasks.Add(text);
+				TaskExtry.Text = "";
+			};
 		}
 	}
 }
